ca40aef58f05507b40747d405b264298340bee36
TekniqueJoseph <joejacksun@gmail.com>
2023-04-13 15:57:33 -0400

    cleaned up js

--- script.js
+++ script.js
@@ -5,13 +5,90 @@
 // Promise. allSettled() is typically used when you have multiple asynchronous 
 // tasks that are not dependent on one another to complete successfully, or you'd always like to know the result of each promise.
 
-const promiseOne = new Promise((resolve, reject) =>
-    setTimeout(resolve, 3000))
-    const promiseTwo = new Promise((resolve, reject) =>
-    setTimeout(reject, 3000))
+// const promiseOne = new Promise((resolve, reject) =>
+//     setTimeout(resolve, 3000))
+//     const promiseTwo = new Promise((resolve, reject) =>
+//     setTimeout(reject, 3000))
 
-    Promise.all([promiseOne, promiseTwo]).then(data => console.log(data))
-    .catch(e => console.log('the 4th chamber', e))
+//     Promise.all([promiseOne, promiseTwo]).then(data => console.log(data))
+//     .catch(e => console.log('the 4th chamber', e))
 
     // Promise.allSettled([promiseOne, promiseTwo]).then(data => console.log(data))
     // .catch(e => console.log('the 4th chamber', e))
+
+       window.addEventListener("hashchange", function() {
+    scrollBy(0, -55);
+});
+
+particlesJS("particles-js", {
+    particles: {
+        number: { value: 80, density: { enable: true, value_area: 800 } },
+        color: { value: "#000000" },
+        shape: {
+            type: "circle",
+            stroke: { width: 0, color: "#000000" },
+            polygon: { nb_sides: 5 },
+            image: { src: "img/github.svg", width: 100, height: 100 }
+        },
+        opacity: {
+            value: 0.5,
+            random: false,
+            anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false }
+        },
+        size: {
+            value: 3,
+            random: true,
+            anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+        },
+        line_linked: {
+            enable: true,
+            distance: 150,
+            color: "#000000",
+            opacity: 0.4,
+            width: 1
+        },
+        move: {
+            enable: true,
+            speed: 6,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: { enable: false, rotateX: 600, rotateY: 1200 }
+        }
+    },
+    interactivity: {
+        detect_on: "canvas",
+        events: {
+            onhover: { enable: true, mode: "repulse" },
+            onclick: { enable: true, mode: "push" },
+            resize: true
+        },
+        modes: {
+            grab: { distance: 400, line_linked: { opacity: 1 } },
+            bubble: { distance: 400, size: 60, duration: 2, opacity: 8, speed: 3 },
+            repulse: { distance: 100, duration: 0.4 },
+            push: { particles_nb: 4 },
+            remove: { particles_nb: 2 }
+        }
+    },
+    retina_detect: true
+});
+var count_particles, stats, update;
+stats = new stats();
+stats.setMode(0);
+stats.domElement.style.position = "absolute";
+stats.domElement.style.left = "0px";
+stats.domElement.style.top = "0px";
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector(".js-count-particles");
+update = function() {
+    stats.begin();
+    stats.end();
+    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+        count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+    }
+    requestAnimationFrame(update);
+};
+requestAnimationFrame(update);
